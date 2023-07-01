c9e94cfc76eb09f5f616ffba2eba0ab24686ccbd
kent10ou <kent10ou@gmail.com>
2023-07-01 15:00:18 -0700

    added some comments

--- valid_words.py
+++ valid_words.py
@@ -1,25 +1,27 @@
 from collections import Counter
 
+# returns a list of valid English word from a given input string
 def valid_words(input_string, wordDict):
     wordSet = set(wordDict)
     subsets = []
     permutations = []
     res = []
     
-    # get subsets for input string 
+    # find all subsets excluding duplicates
     find_subsets(input_string, [], 0, subsets)
 
-    # for each subset get the permutations and check for duplicates
+    # for each subset get the permutations 
     for subset in subsets:
         permute(subset, permutations)
     
+    # check if word in permutations is a valid English word
     for word in permutations:
-        # check if word is valid English word
         if word in wordSet:
             res.append(word)
 
     return res
 
+# returns the subsets for a given input string and exclude duplicates -> List[List[string]]
 def find_subsets(s, subset, i, subsets):
     if i >= len(s):            
         subsets.append(subset[::])
@@ -35,6 +37,7 @@ def find_subsets(s, subset, i, subsets):
         
     find_subsets(s, subset, i + 1, subsets)
 
+# returns the permutations of word and join the string -> List[string]
 def permute(word, res):
     perm = []
     count = Counter(word)
@@ -64,6 +67,9 @@ words = valid_words(input_string, dictionary)
 print(words)
 
 '''
+subsets: [['o', 'o', 'g', 'd'], ['o', 'o', 'g'], ['o', 'o', 'd'], ['o', 'o'], ['o', 'g', 'd'], ['o', 'g'], ['o', 'd'], ['o'], ['g', 'd'], ['g'], ['d'], []]
+permutations: ['oogd', 'oodg', 'ogod', 'ogdo', 'odog', 'odgo', 'good', 'godo', 'gdoo', 'doog', 'dogo', 'dgoo', 'oog', 'ogo', 'goo', 'ood', 'odo', 'doo', 'oo', 'ogd', 'odg', 'god', 'gdo', 'dog', 'dgo', 'og', 'go', 'od', 'do', 'o', 'gd', 'dg', 'g', 'd', '']
+
 We can also go with Python library itertools powerset O(2^n), then find permutations O(n!) of the subsets, and finally check against valid English words. 
 Though I think the best solution to this if we know all possible words is to build a trie O(n) then search words using the given characters. Searching a Trie would be fastest O(k)
 '''
